eea1ee0ab08e51a94a21a229a4be1cc5443e68a8
Matthew Fernandez <matthew.fernandez@gmail.com>
2011-06-22 15:37:34 +1000

    Add install.sh script.

---
+++ internal/install.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# This script symlinks all relevant scripts from this repository into ~/bin and
+# configuration settings into your home directory.
+
+# Set verbose and make a failure terminate the script
+set -e
+
+REPO=`dirname $0`
+REPO=`readlink -f "${REPO}/.."`
+
+mkdir -p "${HOME}/bin"
+
+# Link useful scripts.
+for i in generate-passwd \
+         github-ls.sh \
+         has-changed.sh \
+         have-lib.sh \
+         playtime \
+         toggle-screensaver; do
+    if [ ! -e "${REPO}/$i" ]; then
+        echo "Error: $i not found in working directory." >&2
+        exit 1
+    fi
+    if [ ! -e "${HOME}/bin/$i" ]; then
+        ln -s "${REPO}/$i" "${HOME}/bin/$i"
+    elif [ ! -L "${HOME}/bin/$i" ]; then
+        echo "Warning: Skipping $i that already exists." >&2
+    fi
+done
+if [ ":${PATH}:" != *":${HOME}/bin:"* ]; then
+    echo "Warning: ${HOME}/bin, where symlinks are created, is not in your \$PATH." >&2
+fi
+
+# Link configurations.
+for i in .dircolors \
+         .gdbinit \
+         .hgrc \
+         .screenrc \
+         .vimrc \
+         .zshrc; do
+    if [ ! -e "${REPO}/config/$i" ]; then
+        echo "Error: $i not found in working directory." >&2
+        exit 1
+    fi
+    if [ ! -e "${HOME}/$i" ]; then
+        ln -s "${REPO}/config/$i" "${HOME}/$i"
+    elif [ ! -L "${HOME}/$i" ]; then
+        echo "Warning: Skipping $i that already exists." >&2
+    fi
+done
+
+# Link HTTPS Everywhere rules.
+if [ `find "${HOME}/.mozilla" -type d -name HTTPSEverywhereUserRules | wc -l` -ne 1 ]; then
+    echo "Error: could not determine your HTTPS Everwhere rules directory." >&2
+else
+    HTTPS_RULES=`find "${HOME}/.mozilla" -type d -name HTTPSEverywhereUserRules`
+    for i in `find "${REPO}/config/HTTPSEverywhereUserRules" -type f -iname "*.xml" -exec echo "{}" \;`; do
+        DEST=${HTTPS_RULES}/`basename "$i"`
+        if [ ! -e "${DEST}" ]; then
+            ln -s "$i" "${DEST}"
+        else
+            echo "Warning: Skipping HTTPS Everywhere rule "`basename "$i"`" that already exists." >&2
+        fi
+    done
+fi
