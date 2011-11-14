fef73dcad55af564e3be1ba5bf12614891534da3
Matthew Fernandez <matthew.fernandez@gmail.com>
2011-11-14 17:24:59 +1100

    Script for removing Ubuntu games.

---
+++ remove-ubuntu-games.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Uninstall default Ubuntu games and remove the games menu.
+
+apt-get purge \
+    aisleriot \
+    gbrainy \
+    glchess \
+    glines \
+    gnect \
+    gnibbles \
+    gnobots2 \
+    gnome-games \
+    gnome-mahjongg \
+    gnome-sudoku \
+    gnomine \
+    gnotravex \
+    gnotski \
+    gtali \
+    iagno \
+    lightsoff \
+    quadrapassel \
+    swell-foop
