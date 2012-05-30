528589c13f5676accad374294405a45bacfc3d5a
Matthew Fernandez <matthew.fernandez@gmail.com>
2012-05-30 20:30:42 +1000

    xmonad: Remove border on full screen mplayer.
    Why is this not the default gnome config?!?

--- config/.xmonad/xmonad.hs
+++ config/.xmonad/xmonad.hs
@@ -1,7 +1,9 @@
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.Layout.NoBorders
 
 main = do
     xmonad $ gnomeConfig {
-        terminal = "/home/matthew/bin/term"
+        terminal = "/home/matthew/bin/term",
+        layoutHook = smartBorders $ layoutHook gnomeConfig
     }
