9989d23a76a49d772bf58471a992badf75ef8e08
Matthew Fernandez <matthew.fernandez@gmail.com>
2012-06-23 15:51:53 +1000

    xmonad: Float slide show windows.
    TODO: The same for fullscreened PDFs.

--- config/.xmonad/xmonad.hs
+++ config/.xmonad/xmonad.hs
@@ -2,8 +2,14 @@ import XMonad
 import XMonad.Config.Gnome
 import XMonad.Layout.NoBorders
 
+myManageHook = composeAll
+    [ className =? "Soffice" --> doFloat -- LibreOffice presentation slide show window
+    ]
+
 main = do
     xmonad $ gnomeConfig {
         terminal = "/home/matthew/bin/term",
         layoutHook = smartBorders $ layoutHook gnomeConfig
+    } {
+        manageHook = myManageHook <+> manageHook gnomeConfig
     }
