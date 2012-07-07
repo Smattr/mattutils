37652a5f118600f33c24d57cc728ae9977e6fa2c
Matthew Fernandez <matthew.fernandez@gmail.com>
2012-07-07 15:53:14 +1000

    xmonad: Fullscreen YouTube, etc.

--- config/.xmonad/xmonad.hs
+++ config/.xmonad/xmonad.hs
@@ -1,15 +1,18 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Layout.NoBorders
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageHelpers
 
 myManageHook = composeAll
     [ className =? "Soffice" --> doFloat -- LibreOffice presentation slide show window
+    , isFullscreen --> doFullFloat -- YouTube, evince, etc. fullscreen
     ]
 
 main = do
     xmonad $ gnomeConfig {
-        terminal = "/home/matthew/bin/term",
-        layoutHook = smartBorders $ layoutHook gnomeConfig
-    } {
-        manageHook = myManageHook <+> manageHook gnomeConfig
+        terminal = "/home/matthew/bin/term"
+      , layoutHook = smartBorders $ layoutHook gnomeConfig
+      , handleEventHook    = fullscreenEventHook
+      , manageHook = myManageHook <+> manageHook gnomeConfig
     }
