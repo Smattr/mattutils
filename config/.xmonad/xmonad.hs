46cb6dcd2cdb01bcd4afb6f4a1e1b54ee1a23060
Matthew Fernandez <matthew.fernandez@gmail.com>
2013-02-19 08:47:28 +1100

    xmonad: Add some nicer shortcuts.

--- config/.xmonad/xmonad.hs
+++ config/.xmonad/xmonad.hs
@@ -3,16 +3,25 @@ import XMonad.Config.Gnome
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
+import XMonad.StackSet
+import XMonad.Util.EZConfig
+import XMonad.Actions.CycleWS
 
 myManageHook = composeAll
     [ className =? "Soffice" --> doFloat -- LibreOffice presentation slide show window
     , isFullscreen --> doFullFloat -- YouTube, evince, etc. fullscreen
     ]
 
-main = do
+main =
     xmonad $ gnomeConfig {
         terminal = "/home/matthew/bin/term"
       , layoutHook = smartBorders $ layoutHook gnomeConfig
       , handleEventHook    = fullscreenEventHook
       , manageHook = myManageHook <+> manageHook gnomeConfig
     }
+    `additionalKeysP`
+    [ ("M-<Up>", windows swapUp)
+    , ("M-<Down>", windows swapDown)
+    , ("M-<Right>", shiftToNext)
+    , ("M-<Left>", shiftToPrev)
+    ]
