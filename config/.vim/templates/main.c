4634a66e01cc6eca7c15aa6c881d7ba2a11d2d57
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-05-07 13:16:43 +1000

    vim: More includes for the C template.

--- config/.vim/templates/main.c
+++ config/.vim/templates/main.c
@@ -1,9 +1,14 @@
 #include <assert.h>
+#include <fcntl.h>
 #include <limits.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
 
