15c9317de46ada3f17b3858c77ed2531a914b3cd
Matthew Fernandez <matthew.fernandez@gmail.com>
2016-09-01 20:30:10 +1000

    rerebase: Strip comments after saving the picklist.
    The way Git's interactive rebase is implemented in shell scripting results in
    incredibly slow rebases when it is given a picklist with many comment lines.
    Unfortunately this meant rerebase often triggered pathological rebases. E.g. one
    I had to side step today that ran for 10 minutes without making it through a
    single commit.
    
    This commit refactors rerebase to strip comments (both those it has introduced
    and the comments Git itself inserted) before handing the picklist back to Git.
    The result is our interactive rebases are back up to speed.
    
    Note that my original observation on Github #23 that Git has some kind of no-op
    detection logic was actually mistaken. There seems to be no such logic, I just
    happened to be working on a fast SSD with a set of commits that applied so fast
    it appeared instantaneous.
    
    Closes Github #23.

--- rerebase.c
+++ rerebase.c
@@ -55,6 +55,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <regex.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -93,58 +94,29 @@ static int move(const char *oldpath, const char *newpath) {
     return 0;
 }
 
-int main(int argc, char **argv) {
-    FILE *in, *out;
-    char *out_file, *line = NULL;
-    size_t line_sz;
-
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s rebase-file\n", argv[0]);
-        goto fail;
-    }
-
-    in = fopen(argv[1], "r");
-    if (in == NULL) {
-        perror("failed to open file");
-        goto fail;
-    }
-
-    // open a temporary file for making a new rebase input
-    {
-        const char *tmp = getenv("TMPDIR");
-        if (tmp == NULL)
-            tmp = "/tmp";
-        if (asprintf(&out_file, "%s/tmp.XXXXXX", tmp) < 0) {
-            fprintf(stderr, "failed to create temporary path\n");
-            goto fail1;
-        }
-    }
-    {
-        int out_fd = mkstemp(out_file);
-        if (out_fd < 0) {
-            perror("failed to open temporary file");
-            goto fail2;
-        }
-        out = fdopen(out_fd, "w");
-        assert(out != NULL);
-    }
+// Insert diffs in-between lines in a rebase picklist
+static int insert_diffs(FILE *in, FILE *out) {
+    regex_t pick;
+    bool regex_inited = false;
+    int ret = 0;
+    char *line = NULL;
 
     // we'll use this to look for lines representing commits in the input
-    regex_t pick;
-    if (regcomp(&pick, "^(pick|fixup|squash) ([[:xdigit:]]+) ", REG_EXTENDED) < 0) {
-        fprintf(stderr, "failed to compile regular expression\n");
-        goto fail3;
-    }
+    ret = regcomp(&pick, "^(pick|fixup|squash) ([[:xdigit:]]+) ", REG_EXTENDED);
+    if (ret != 0)
+        goto end;
+    regex_inited = true;
 
+    size_t line_sz;
     for (;;) {
 
+        errno = 0;
         ssize_t r = getline(&line, &line_sz, in);
         if (r < 0) {
-            int err = errno;
+            ret = -errno;
             if (feof(in))
                 break;
-            fprintf(stderr, "failed to read from input file: %s", strerror(err));
-            goto fail4;
+            goto end;
         }
 
         regmatch_t match[3];
@@ -159,102 +131,242 @@ int main(int argc, char **argv) {
 
             // Write the line with a Vim marker to indicate the start of a fold
             if (fprintf(out, "%.*s {{{\n", (int)r, line) < 0) {
-                fprintf(stderr, "failed to write to output file\n");
-                goto fail4;
+                ret = -1;
+                goto end;
             }
 
             // Create a Git command to get a diff of this commit
             char *command;
             if (asprintf(&command, "git show %.*s", match[2].rm_eo - match[2].rm_so,
                     &line[match[2].rm_so]) < 0) {
-                fprintf(stderr, "asprintf failed\n");
-                goto fail4;
+                ret = -ENOMEM;
+                goto end;
             }
 
             // Retrieve the diff and write it to the output
+            errno = 0;
             FILE *pipe = popen(command, "r");
+            if (pipe == NULL)
+                ret = errno == 0 ? -ENOMEM : -errno;
             free(command);
-            if (pipe == NULL) {
-                fprintf(stderr, "failed to open Git pipe\n");
-                goto fail4;
-            }
+            if (ret != 0)
+                goto end;
             char *l = NULL;
             size_t l_sz;
             for (;;) {
-                ssize_t r2 = getline(&l, &l_sz, pipe);
-                if (r2 < 0) {
-                    int err = errno;
+                errno = 0;
+                if (getline(&l, &l_sz, pipe) < 0) {
+                    ret = -errno;
                     if (feof(pipe))
                         break;
-                    fprintf(stderr, "failed to read from Git pipe: %s\n", strerror(err));
                     free(l);
                     pclose(pipe);
-                    goto fail4;
+                    goto end;
                 }
 
                 if (fprintf(out, " # %s", l) < 0) {
-                    fprintf(stderr, "failed to write to output file\n");
+                    ret = -1;
                     free(l);
                     pclose(pipe);
-                    goto fail4;
+                    goto end;
                 }
             }
             free(l);
             int status = pclose(pipe);
-            if (status < 0 || WEXITSTATUS(status) != EXIT_SUCCESS) {
-                fprintf(stderr, "git show failed\n");
-                goto fail4;
+            if (status == -1) {
+                ret = -1;
+                goto end;
+            }
+            if (WEXITSTATUS(status) != EXIT_SUCCESS) {
+                ret = WEXITSTATUS(status);
+                goto end;
             }
 
             // write a fold close marker
             if (fputs(" # }}}\n", out) < 0) {
-                fprintf(stderr, "failed to write to output file\n");
-                goto fail4;
+                ret = -1;
+                goto end;
             }
 
         } else {
             // normal line; write it out as-is
             size_t w = fwrite(line, 1, (size_t)r, out);
             if (w != (size_t)r) {
-                fprintf(stderr, "failed to write to output file\n");
-                goto fail4;
+                ret = -1;
+                goto end;
             }
         }
-
-        continue;
-fail4:
-        free(line);
-        regfree(&pick);
-        goto fail3;
-
     }
-    free(line);
-    regfree(&pick);
 
-    // Overwrite the original input file with our modified version
-    fclose(out);
-    fclose(in);
-    if (move(out_file, argv[1]) < 0) {
-        fprintf(stderr, "failed to overwrite original input\n");
-        unlink(out_file);
-        free(out_file);
+end:
+    free(line);
+    if (regex_inited)
+        regfree(&pick);
+    return ret;
+}
+
+static int strip_comments(FILE *in, FILE *out) {
+    char *line = NULL;
+    size_t line_sz;
+    int ret = 0;
+
+    for (;;) {
+
+        errno = 0;
+        ssize_t r = getline(&line, &line_sz, in);
+        if (r < 0) {
+            ret = -errno;
+            if (feof(in))
+                break;
+            goto end;
+        }
+
+        // Is this a comment?
+        const char *c = line;
+        for (; *c == ' ' || *c == '\t'; c++);
+        if (*c == '\0' || *c == '\n' || *c == '#')
+            continue;
+
+        size_t w = fwrite(line, 1, r, out);
+        if (w != (size_t)r) {
+            ret = -1;
+            goto end;
+        }
+    }
+
+end:
+    free(line);
+    return ret;
+}
+
+int main(int argc, char **argv) {
+    FILE *in, *out;
+    char *out_file;
+    const char *tmp;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s rebase-file\n", argv[0]);
         return EXIT_FAILURE;
     }
-    free(out_file);
+
+    in = fopen(argv[1], "r");
+    if (in == NULL) {
+        perror("failed to open file");
+        return EXIT_FAILURE;
+    }
+
+    // open a temporary file for making a new rebase input
+    tmp = getenv("TMPDIR");
+    if (tmp == NULL)
+        tmp = "/tmp";
+    if (asprintf(&out_file, "%s/tmp.XXXXXX", tmp) < 0) {
+        fprintf(stderr, "failed to create temporary path\n");
+        fclose(in);
+        return EXIT_FAILURE;
+    }
+    {
+        int out_fd = mkstemp(out_file);
+        if (out_fd < 0) {
+            perror("failed to open temporary file");
+            free(out_file);
+            fclose(in);
+            return EXIT_FAILURE;
+        }
+        out = fdopen(out_fd, "w");
+        assert(out != NULL);
+    }
+
+    {
+        int ret = insert_diffs(in, out);
+        fclose(out);
+        fclose(in);
+        if (ret != 0) {
+            fprintf(stderr, "failed to insert diffs: %s\n", strerror(-ret));
+            unlink(out_file);
+            free(out_file);
+            return EXIT_FAILURE;
+        }
+    }
+
+    // Overwrite the original input file with our modified version
+    {
+        int ret = move(out_file, argv[1]);
+        if (ret < 0)
+            unlink(out_file);
+        free(out_file);
+        if (ret < 0) {
+            fprintf(stderr, "failed to overwrite original input\n");
+            return EXIT_FAILURE;
+        }
+    }
 
     // Now open Vim with the new file with all folds collapsed
-    char *args[] = { "vim", "+set foldmethod=marker", "+set foldlevel=0", argv[1], NULL };
-    execvp("vim", args);
-    perror("failed exec");
-    return EXIT_FAILURE;
+    {
+        char *command;
+        if (asprintf(&command, "vim '+set foldmethod=marker' '+set foldlevel=0' '%s'", argv[1])
+                < 0) {
+            fprintf(stderr, "failed to construct Vim command\n");
+            return EXIT_FAILURE;
+        }
+        int ret = system(command);
+        free(command);
+        if (ret == -1) {
+            fprintf(stderr, "executing Vim failed\n");
+            return EXIT_FAILURE;
+        }
+        if (WEXITSTATUS(ret) != EXIT_SUCCESS)
+            return WEXITSTATUS(ret);
+    }
 
-fail3:
-    fclose(out);
-    unlink(out_file);
-fail2:
-    free(out_file);
-fail1:
-    fclose(in);
-fail:
-    return EXIT_FAILURE;
+    /* Now strip the comment lines. This is not strictly necessary, but Git's interactive rebase
+     * script seems incredibly slow to pass over long stretches of comment lines.
+     */
+
+    in = fopen(argv[1], "r");
+    if (in == NULL) {
+        perror("failed to open file");
+        return EXIT_FAILURE;
+    }
+
+    if (asprintf(&out_file, "%s/tmp.XXXXXX", tmp) < 0) {
+        fprintf(stderr, "failed to create temporary path\n");
+        fclose(in);
+        return EXIT_FAILURE;
+    }
+    {
+        int out_fd = mkstemp(out_file);
+        if (out_fd < 0) {
+            perror("failed to open temporary file");
+            free(out_file);
+            fclose(in);
+            return EXIT_FAILURE;
+        }
+        out = fdopen(out_fd, "w");
+        assert(out != NULL);
+    }
+
+    {
+        int ret = strip_comments(in, out);
+        fclose(out);
+        fclose(in);
+        if (ret != 0) {
+            fprintf(stderr, "failed to strip comments: %s\n", strerror(-ret));
+            unlink(out_file);
+            free(out_file);
+            return EXIT_FAILURE;
+        }
+    }
+
+    {
+        int ret = move(out_file, argv[1]);
+        if (ret < 0)
+            unlink(out_file);
+        free(out_file);
+        if (ret < 0) {
+            fprintf(stderr, "failed to overwrite original input\n");
+            return EXIT_FAILURE;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
