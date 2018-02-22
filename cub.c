d7b5c3ffabc23b0e2ca4aed42c374f74456c2511
Matthew Fernandez <matthew.fernandez@gmail.com>
2018-02-21 21:18:46 -0800

    cub: implement negation of self check
    Related to Github #32 "port cub to C"

--- cub.c
+++ cub.c
@@ -40,9 +40,8 @@ static int undef_shift_check(struct analysis *this, const char *filename,
   if (regexec(this->state, line, match_sz, match, 0) == 0) {
     if (strtoul(line + match[1].rm_so, NULL, 10) >= 31) {
       fprintf(stderr,
-        "%s:%u: %s%s"
-        "potential undefined left shift\n", filename, lineno, line,
-        strlen(line) > 0 && line[strlen(line) - 1] == '\n' ? "" : "\n");
+        "%s:%u: %s\n"
+        "potential undefined left shift\n", filename, lineno, line);
       return -1;
     }
   }
@@ -56,11 +55,61 @@ static void undef_shift_deinit(struct analysis *this) {
   regfree(this->state);
 }
 
+static int negate_self_init(struct analysis *this) {
+  assert(this != NULL);
+
+  regex_t *r = malloc(sizeof(*r));
+  if (r == NULL)
+    return -1;
+
+  if (regcomp(r, "([[:alpha:]_][[:alnum:]]*)[[:space:]]*=[[:space:]]*-"
+        "[[:space:]]*([[:alpha:]_][[:alnum:]]*)", REG_EXTENDED) != 0) {
+    free(r);
+    return -1;
+  }
+
+  this->state = r;
+  return 0;
+}
+
+static int negate_self_check(struct analysis *this, const char *filename,
+    unsigned lineno, const char *line) {
+  assert(this != NULL);
+  assert(this->state != NULL);
+
+  regmatch_t match[3];
+  static const size_t match_sz = sizeof(match) / sizeof(match[0]);
+  if (regexec(this->state, line, match_sz, match, 0) == 0) {
+    const char *s1 = line + match[1].rm_so;
+    size_t len1 = match[1].rm_eo - match[1].rm_so;
+    const char *s2 = line + match[2].rm_so;
+    size_t len2 = match[2].rm_eo - match[2].rm_so;
+    if (len1 == len2 && strncmp(s1, s2, len1) == 0) {
+      fprintf(stderr, "%s:%u: %s\n potential negation of INT_MIN\n", filename,
+        lineno, line);
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
+static void negate_self_deinit(struct analysis *this) {
+  assert(this != NULL);
+  assert(this->state != NULL);
+  regfree(this->state);
+}
+
 struct analysis ANALYSES[] = {
   {
     .init = undef_shift_init,
     .check = undef_shift_check,
     .deinit = undef_shift_deinit,
+  },
+  {
+    .init = negate_self_init,
+    .check = negate_self_check,
+    .deinit = negate_self_deinit,
   },
 };
 
@@ -88,6 +137,10 @@ static int scan_file(const char *path) {
       }
       break;
     }
+
+    // Strip trailing \n to make it easier for analyses
+    if (line[strlen(line) - 1] == '\n')
+      line[strlen(line) - 1] = '\0';
 
     // Run analysis passes
     for (size_t i = 0; i < sizeof(ANALYSES) / sizeof(ANALYSES[0]); i++)
