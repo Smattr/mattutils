a13477da8fddacb8ef2fc56e37633c3f2aa2bb8f
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-04-30 08:30:22 -0700

    fwdmail: two spaces for indent

--- fwdmail.py
+++ fwdmail.py
@@ -38,85 +38,85 @@ import syslog
 import time
 
 def main(argv, stdout, stderr):
-    # Parse command line arguments.
-    parser = argparse.ArgumentParser(prog='fwdmail.py',
-        description='Forward local mail to another address')
-    parser.add_argument('--check_connection', action='store_true',
-        help='Exit with success if offline')
-    parser.add_argument('--from_address', '-f', required=True,
-        help='From address to use when forwarding')
-    parser.add_argument('--login',
-        help='Login name if authentication is required for sending')
-    parser.add_argument('--mbox', '-m', default='/var/mail/%s' % getpass.getuser(),
-        help='Mbox file to open if not the default')
-    parser.add_argument('--port', '-p', default=25, type=int,
-        help='Port to send through')
-    parser.add_argument('--password',
-        help='Password if authentication is required for sending')
-    parser.add_argument('--server', '-s', required=True,
-        help='SMTP server to forward messages through')
-    parser.add_argument('--to', '-t', required=True,
-        help='Address to forward to')
-    parser.add_argument('--tls', action='store_true',
-        help='Use TLS security')
+  # Parse command line arguments.
+  parser = argparse.ArgumentParser(prog='fwdmail.py',
+    description='Forward local mail to another address')
+  parser.add_argument('--check_connection', action='store_true',
+    help='Exit with success if offline')
+  parser.add_argument('--from_address', '-f', required=True,
+    help='From address to use when forwarding')
+  parser.add_argument('--login',
+    help='Login name if authentication is required for sending')
+  parser.add_argument('--mbox', '-m', default='/var/mail/%s' % getpass.getuser(),
+    help='Mbox file to open if not the default')
+  parser.add_argument('--port', '-p', default=25, type=int,
+    help='Port to send through')
+  parser.add_argument('--password',
+    help='Password if authentication is required for sending')
+  parser.add_argument('--server', '-s', required=True,
+    help='SMTP server to forward messages through')
+  parser.add_argument('--to', '-t', required=True,
+    help='Address to forward to')
+  parser.add_argument('--tls', action='store_true',
+    help='Use TLS security')
 
-    p = parser.parse_args(argv[1:])
+  p = parser.parse_args(argv[1:])
 
-    hostname = socket.gethostname()
+  hostname = socket.gethostname()
 
-    # Check we're part of expected groups.
-    for gid in os.getgroups():
-        if grp.getgrgid(gid).gr_name == 'mail':
-            break
-    else:
-        stderr('Warning: you are not part of the \'mail\' group')
+  # Check we're part of expected groups.
+  for gid in os.getgroups():
+    if grp.getgrgid(gid).gr_name == 'mail':
+      break
+  else:
+    stderr('Warning: you are not part of the \'mail\' group')
 
-    # Open the local mailbox.
-    box = None
+  # Open the local mailbox.
+  box = None
+  try:
+    box = mailbox.mbox(p.mbox)
+  except Exception as inst:
+    stderr(f'Failed to open {p.mbox}: {inst}')
+    return 1
+
+  smtp = None
+
+  # Forward and delete each message.
+  try:
+    box.lock()
+  except Exception as inst:
+    stderr(f'Failed to lock mailbox file: {inst}')
+    return -1
+  for i in itertools.count():
+
     try:
-        box = mailbox.mbox(p.mbox)
-    except Exception as inst:
-        stderr(f'Failed to open {p.mbox}: {inst}')
+      key, msg = box.popitem()
+    except KeyError:
+      # mailbox empty
+      break
+
+    if not smtp:
+      # Connect to the SMTP server.
+      try:
+        try:
+          smtp = smtplib.SMTP(p.server, p.port)
+          smtp.connect(p.server, p.port)
+        except Exception as inst:
+          if p.check_connection:
+            box.unlock()
+            return 0
+          else:
+            raise inst
+        if p.tls:
+          smtp.starttls()
+        if p.login:
+          smtp.login(p.login, p.password)
+      except Exception as inst:
+        stderr(f'Failed to connect to {p.server}: {inst}')
+        box.unlock()
         return 1
-
-    smtp = None
-
-    # Forward and delete each message.
     try:
-        box.lock()
-    except Exception as inst:
-        stderr(f'Failed to lock mailbox file: {inst}')
-        return -1
-    for i in itertools.count():
-
-        try:
-            key, msg = box.popitem()
-        except KeyError:
-            # mailbox empty
-            break
-
-        if not smtp:
-            # Connect to the SMTP server.
-            try:
-                try:
-                    smtp = smtplib.SMTP(p.server, p.port)
-                    smtp.connect(p.server, p.port)
-                except Exception as inst:
-                    if p.check_connection:
-                        box.unlock()
-                        return 0
-                    else:
-                        raise inst
-                if p.tls:
-                    smtp.starttls()
-                if p.login:
-                    smtp.login(p.login, p.password)
-            except Exception as inst:
-                stderr(f'Failed to connect to {p.server}: {inst}')
-                box.unlock()
-                return 1
-        try:
-            smtp.sendmail(p.from_address, p.to, \
+      smtp.sendmail(p.from_address, p.to, \
 f"""From: {p.from_address}
 To: {p.to}
 Subject: {hostname}: {msg['Subject'] or ''}
@@ -124,32 +124,32 @@ Subject: {hostname}: {msg['Subject'] or ''}
 Forwarded email from {hostname}:{p.mbox}:
 
 {msg or ''}""")
-            box.flush()
-        except Exception as inst:
-            stderr(f'Failed to send/delete message {key}: {inst}')
-            try:
-                smtp.quit()
-            except:
-                pass # Ignore.
-            box.unlock()
-            return -1
-        # pause every 10 emails to avoid flooding the SMTP server
-        if i % 10 == 9:
-            time.sleep(0.5)
-    if smtp: smtp.quit()
-    box.unlock()
+      box.flush()
+    except Exception as inst:
+      stderr(f'Failed to send/delete message {key}: {inst}')
+      try:
+        smtp.quit()
+      except:
+        pass # Ignore.
+      box.unlock()
+      return -1
+    # pause every 10 emails to avoid flooding the SMTP server
+    if i % 10 == 9:
+      time.sleep(0.5)
+  if smtp: smtp.quit()
+  box.unlock()
 
-    return 0
+  return 0
 
 if __name__ == '__main__':
-    if sys.stdout.isatty():
-        def stdout(msg):
-            sys.stdout.write(f'{msg}\n')
-        def stderr(msg):
-            sys.stderr.write(f'{msg}\n')
-    else:
-        # We seem to be running from a crontab.
-        syslog.openlog('fwdmail', syslog.LOG_PID, syslog.LOG_MAIL)
-        stdout = syslog.syslog
-        stderr = functools.partial(syslog.syslog, syslog.LOG_ERR)
-    sys.exit(main(sys.argv, stdout, stderr))
+  if sys.stdout.isatty():
+    def stdout(msg):
+      sys.stdout.write(f'{msg}\n')
+    def stderr(msg):
+      sys.stderr.write(f'{msg}\n')
+  else:
+    # We seem to be running from a crontab.
+    syslog.openlog('fwdmail', syslog.LOG_PID, syslog.LOG_MAIL)
+    stdout = syslog.syslog
+    stderr = functools.partial(syslog.syslog, syslog.LOG_ERR)
+  sys.exit(main(sys.argv, stdout, stderr))
