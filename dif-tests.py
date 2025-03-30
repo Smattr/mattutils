210fa6bb2db7efb5fce1893703681c6198b08393
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-03-30 13:04:41 -0700

    add a basic test suite for dif.c

---
+++ dif-tests.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+"""
+tests for dif.c
+"""
+
+import re
+from pathlib import Path
+
+import pexpect
+import pytest
+
+
+@pytest.mark.xfail(strict=True)
+def test_unicode_prefix(tmp_path: Path):
+    """are non-ASCII characters shown correctly when their prefix matches?"""
+
+    a = tmp_path / "a"
+    a.write_text("foo ┌─┐\n", encoding="utf-8")
+
+    b = tmp_path / "b"
+    b.write_text("foo ┌──┐ \n", encoding="utf-8")
+
+    proc = pexpect.spawn("dif", [str(a), str(b)], encoding="utf-8", dimensions=(4, 80))
+    proc.expect(re.compile(r"^\[31m-.*┌.*┐", flags=re.MULTILINE))
+    proc.expect(re.compile(r"^\[32m+.*┌.*┐", flags=re.MULTILINE))
+    proc.expect(pexpect.EOF)
+
+
+@pytest.mark.xfail(strict=True)
+def test_unicode_suffix(tmp_path: Path):
+    """are non-ASCII characters shown correctly when their suffix matches?"""
+
+    a = tmp_path / "a"
+    a.write_text("foo ┌─┐\n", encoding="utf-8")
+
+    b = tmp_path / "b"
+    b.write_text("foo ┌──┐\n", encoding="utf-8")
+
+    proc = pexpect.spawn("dif", [str(a), str(b)], encoding="utf-8", dimensions=(4, 80))
+    proc.expect(re.compile(r"^\[31m-.*┌.*┐", flags=re.MULTILINE))
+    proc.expect(re.compile(r"^\[32m+.*┌.*┐", flags=re.MULTILINE))
+    proc.expect(pexpect.EOF)
