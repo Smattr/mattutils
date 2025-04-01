c3cc055e6db8262720d43f7da915905bb3bbe2da
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-03-31 17:10:55 -0700

    dif: add a test for '^++[^+]'
    This checks for the problem that was fixed in
    2b27339bce8ba9a39eaba6f7c2df458ca271a1ec.

--- dif-tests.py
+++ dif-tests.py
@@ -11,6 +11,21 @@ import pexpect
 import pytest
 
 
+def test_plus_plus(tmp_path: Path):
+    """
+    are lines beginning with e.g. `++[^+]` correctly detected as not part of the header?
+    """
+    a = tmp_path / "a"
+    a.write_text("-foo\n", encoding="utf-8")
+
+    b = tmp_path / "b"
+    b.write_text("++bar\n", encoding="utf-8")
+
+    proc = pexpect.spawn("dif", [str(a), str(b)], encoding="utf-8")
+    proc.expect(re.compile(r"^\[32m++", flags=re.MULTILINE))
+    proc.expect(pexpect.EOF)
+
+
 def test_unicode_prefix(tmp_path: Path):
     """are non-ASCII characters shown correctly when their prefix matches?"""
 
