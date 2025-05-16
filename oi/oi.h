31001d4db4df59480ae8f8b4738428b5382bb076
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-05-15 17:03:47 -0700

    oi: merge “x = <value of x>” use case into 'oi' macro
    The original goal was to have everything accessible through a single `oi` macro
    but I was insufficiently creative to figure out how to do it at the time. The
    form of this implementation probably explains why. The gymnastic lengths this
    new macro goes to are mainly in service of (1) retaining the `printf` format
    attribute, without which malformed calls are too easy to make, and (2) avoiding
    -Wformat-extra-args compiler warnings that are triggered by most naive
    alternatives.

--- oi/oi.h
+++ oi/oi.h
@@ -15,6 +15,7 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 // only include unistd.h conditionally so we have a hope of compiling on
@@ -32,6 +33,80 @@ static __thread int oi_printed_;
 static __thread const char *oi_filename_;
 static __thread int oi_lineno_;
 
+/// a value of arbitrary type
+///
+/// It is assumed there is an external way of discriminating which of the union
+/// members is in use.
+struct oi_value_ {
+  union {
+    long long signed_value;
+    unsigned long long unsigned_value;
+    double double_value;
+    const char *char_ptr_value;
+  };
+};
+
+/// construct a `oi_value_` with a signed value
+///
+/// This function is not expected to be called directly by users. It is only
+/// expected to be called from the `oi` macro.
+static inline struct oi_value_ oi_make_signed_(long long value) {
+  struct oi_value_ v;
+  v.signed_value = value;
+  return v;
+}
+
+/// construct a `oi_value_` with an unsigned value
+///
+/// This function is not expected to be called directly by users. It is only
+/// expected to be called from the `oi` macro.
+static inline struct oi_value_ oi_make_unsigned_(unsigned long long value) {
+  struct oi_value_ v;
+  v.unsigned_value = value;
+  return v;
+}
+
+/// construct a `oi_value_` with a double value
+///
+/// This function is not expected to be called directly by users. It is only
+/// expected to be called from the `oi` macro.
+static inline struct oi_value_ oi_make_double_(double value) {
+  struct oi_value_ v;
+  v.double_value = value;
+  return v;
+}
+
+/// construct a `oi_value_` with a string value
+///
+/// This function is not expected to be called directly by users. It is only
+/// expected to be called from the `oi` macro.
+static inline struct oi_value_ oi_make_char_ptr_(const char *value) {
+  struct oi_value_ v;
+  v.char_ptr_value = value;
+  return v;
+}
+
+/// construct a `oi_value_`
+///
+/// This macro is not expected to be called directly by users. It is only
+/// expected to be called from the `oi` macro.
+#define oi_make_value_(v)                                                      \
+  (_Generic((v),                                                               \
+       signed char: oi_make_signed_,                                           \
+       short: oi_make_signed_,                                                 \
+       int: oi_make_signed_,                                                   \
+       long: oi_make_signed_,                                                  \
+       long long: oi_make_signed_,                                             \
+       unsigned char: oi_make_unsigned_,                                       \
+       unsigned short: oi_make_unsigned_,                                      \
+       unsigned: oi_make_unsigned_,                                            \
+       unsigned long: oi_make_unsigned_,                                       \
+       unsigned long long: oi_make_unsigned_,                                  \
+       float: oi_make_double_,                                                 \
+       double: oi_make_double_,                                                \
+       char *: oi_make_char_ptr_,                                              \
+       const char *: oi_make_char_ptr_)(v))
+
 /// setup for a debugging print
 ///
 /// This function is not expected to be called directly by users. It is only
@@ -45,20 +120,28 @@ static inline void oi_open_(const char *filename, int lineno) {
   oi_lineno_ = lineno;
 }
 
+// `oi_print_`, `oi_signed_`, `oi_unsigned_`, `oi_double_`, and `oi_char_ptr_`
+// are expected to conform to the same type signature. This is necessary to be
+// able to branch to them from the `oi` macro.
+
 /// do a debugging print
 ///
 /// This function is not expected to be called directly by users. It is only
 /// expected to be called from the `oi` macro.
 ///
+/// @param ignored This parameter is unused
 /// @param format A printf-style format string
+/// @param ignored2 This parameter is unused
 /// @param ... printf-style format arguments
-static inline __attribute__((format(printf, 1, 2))) void
-oi_print_(const char *format, ...) {
-  assert(oi_printed_ == 0);
+static inline __attribute__((format(printf, 2, 4))) void
+oi_print_(const char *ignored, const char *format, struct oi_value_ ignored2,
+          ...) {
+  (void)ignored;
+
   oi_printed_ = 1;
 
   va_list ap;
-  va_start(ap, format);
+  va_start(ap, ignored2);
 
   int use_colour = 0;
 #ifdef __has_include
@@ -85,36 +168,139 @@ oi_print_(const char *format, ...) {
   va_end(ap);
 }
 
+/// print a signed value for debugging
+///
+/// This function is not expected to be called directly by users. It is only
+/// expected to be called from the `oi` macro.
+///
+/// @param name Symbol or expression from which this value originated
+/// @param ignored This parameter is unused
+/// @param value Value to print
+/// @param ... These parameters are unused
+static inline void oi_signed_(const char *name, const char *ignored,
+                              struct oi_value_ value, ...) {
+  (void)ignored;
+
+  assert(name != NULL);
+  assert(strcmp(name, "") != 0);
+
+  oi_print_(NULL, "%s == %lld", value, name, value.signed_value);
+
+  // if this could be character data, print its equivalent
+  if (value.signed_value > 31 && value.signed_value < 127)
+    oi_print_(NULL, "(%s == '%c')", value, name, (char)value.signed_value);
+}
+
+/// print an unsigned value for debugging
+///
+/// This function is not expected to be called directly by users. It is only
+/// expected to be called from the `oi` macro.
+///
+/// @param name Symbol or expression from which this value originated
+/// @param ignored This parameter is unused
+/// @param value Value to print
+/// @param ... These parameters are unused
+static inline void oi_unsigned_(const char *name, const char *ignored,
+                                struct oi_value_ value, ...) {
+  (void)ignored;
+
+  assert(name != NULL);
+  assert(strcmp(name, "") != 0);
+
+  oi_print_(NULL, "%s == %llu, 0x%llx", value, name, value.unsigned_value,
+            value.unsigned_value);
+
+  // if this could be character data, print its equivalent
+  if (value.unsigned_value > 31 && value.unsigned_value < 127)
+    oi_print_(NULL, "(%s == '%c')", value, name, (char)value.unsigned_value);
+}
+
+/// print a double value for debugging
+///
+/// This function is not expected to be called directly by users. It is only
+/// expected to be called from the `oi` macro.
+///
+/// @param name Symbol or expression from which this value originated
+/// @param ignored This parameter is unused
+/// @param value Value to print
+/// @param ... These parameters are unused
+static inline void oi_double_(const char *name, const char *ignored,
+                              struct oi_value_ value, ...) {
+  (void)ignored;
+
+  assert(name != NULL);
+  assert(strcmp(name, "") != 0);
+
+  oi_print_(NULL, "%s == %f", value, name, value.double_value);
+}
+
+/// print a string value for debugging
+///
+/// This function is not expected to be called directly by users. It is only
+/// expected to be called from the `oi` macro.
+///
+/// @param name Symbol or expression from which this value originated
+/// @param ignored This parameter is unused
+/// @param value Value to print
+/// @param ... These parameters are unused
+static inline void oi_char_ptr_(const char *name, const char *ignored,
+                                struct oi_value_ value, ...) {
+  (void)ignored;
+
+  assert(name != NULL);
+  assert(strcmp(name, "") != 0);
+
+  oi_print_(NULL, "%s == \"%s\"", value, name, value.char_ptr_value);
+}
+
 /// tear down from a debugging print
 ///
 /// This function is not expected to be called directly by users. It is only
 /// expected to be called from the `oi` macro.
 static inline void oi_close_(void) {
   if (oi_printed_ == 0) {
-    oi_print_("here");
+    struct oi_value_ ignored;
+    ignored.signed_value = 0;
+    oi_print_(NULL, "here", ignored);
   }
   oi_printed_ = 0;
 }
 
+/// no-op
+///
+/// This function exists purely to allow macro expansions that result in
+/// parameter-less expressions `oi__` to be syntactically valid.
+static inline void oi__(void) {}
+
 /// make a debugging print
 ///
-/// The strange form of this macro is intended to support two use cases:
+/// The strange form of this macro is intended to support three use cases:
 ///
 ///   oi("my %s", "message"); // printf-style debugging
 ///   oi; // just print “here” with file and line number information
+///   oi(x); // print “x == <value of x>”
 #define oi                                                                     \
   for (int oi_ = (oi_open_(__FILE__, __LINE__), 1); oi_; oi_close_(), --oi_)   \
-  (void)oi_print_
+  (void)oi__
 
-#define oi_show(expr)                                                          \
-  oi(_Generic((expr),                                                          \
-         const char *: #expr " == \"%s\"",                                     \
-         int: #expr " == %d",                                                  \
-         unsigned: #expr " == %u",                                             \
-         long: #expr " == %ld",                                                \
-         unsigned long: #expr " == %lu",                                       \
-         long long: #expr " == %lld",                                          \
-         unsigned long long: #expr " == %llu",                                 \
-         float: #expr " == %f",                                                \
-         double: #expr " == %f"),                                              \
-     (expr))
+#define oi__(fmt, ...)                                                         \
+  ((#fmt[0] == '"' ? oi_print_                                                 \
+                   : _Generic((fmt),                                           \
+            signed char: oi_signed_,                                           \
+            short: oi_signed_,                                                 \
+            int: oi_signed_,                                                   \
+            long: oi_signed_,                                                  \
+            long long: oi_signed_,                                             \
+            unsigned char: oi_unsigned_,                                       \
+            unsigned short: oi_unsigned_,                                      \
+            unsigned long: oi_unsigned_,                                       \
+            unsigned long long: oi_unsigned_,                                  \
+            float: oi_double_,                                                 \
+            double: oi_double_,                                                \
+            char *: oi_char_ptr_,                                              \
+            const char *: oi_char_ptr_))(                                      \
+      #fmt,                                                                    \
+      __builtin_choose_expr(                                                   \
+          __builtin_types_compatible_p(__typeof__(fmt), char[]), fmt,          \
+          "unused"),                                                           \
+      oi_make_value_(fmt), ##__VA_ARGS__))
