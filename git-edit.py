797f3b247a69c3c7add9ab7f979c729f09fe058e
Matthew Fernandez <matthew.fernandez@gmail.com>
2025-10-28 17:20:02 -0700

    git-edit: support a remote other than 'origin'

--- git-edit.py
+++ git-edit.py
@@ -33,6 +33,7 @@ def main(args: List[str]) -> int:
     # parse command line options
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--onto", help="base branch of the current feature branch")
+    parser.add_argument("--remote", help="remote to compare against", default="origin")
     options = parser.parse_args(args[1:])
 
     # check this is actually a Git repository
@@ -40,7 +41,7 @@ def main(args: List[str]) -> int:
 
     # if the user did not give us a base, figure it out from upstream
     if options.onto is None:
-        show = call(["git", "remote", "show", "origin"])
+        show = call(["git", "remote", "show", options.remote])
         default = re.search(r"^\s*HEAD branch: (.*)$", show, flags=re.MULTILINE)
         if default is None:
             sys.stderr.write("could not figure out default branch name\n")
@@ -48,7 +49,7 @@ def main(args: List[str]) -> int:
         options.onto = default.group(1)
 
     # determine the point at which we branched off the base
-    merge_base = call(["git", "merge-base", f"origin/{options.onto}", "HEAD"])
+    merge_base = call(["git", "merge-base", f"{options.remote}/{options.onto}", "HEAD"])
 
     # run a rebase, assuming we want to use `rerebase`
     run(["git", "re", "--interactive", merge_base])
