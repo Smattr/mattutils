fabbb014f91cc71512b8ae1c31a9b7048bf07d34
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-02-25 20:54:05 -0800

    ute: binary printer

---
+++ libute/libute/src/putb.c
@@ -0,0 +1,28 @@
+/// @file
+/// @brief Print a value in binary format
+///
+/// All content in this file is in the public domain. Use it any way you wish.
+
+#include <ute/print.h>
+#include <limits.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stddef.h>
+
+int putb(const void *data, size_t size, FILE *stream) {
+  assert(data != NULL || size == 0);
+  assert(stream != NULL);
+
+  int written = 0;
+  const char *d = data;
+  for (size_t byte = 0; byte < size; ++byte) {
+    for (size_t bit = 0; bit < CHAR_BIT; ++bit) {
+      const char c = ((d[byte] >> bit) & 1) ? '1' : '0';
+      if (putc(c, stream) < 0)
+        return EOF;
+      ++written;
+    }
+  }
+
+  return written;
+}
---
+++ libute/test/src/test-putb.c
@@ -0,0 +1,47 @@
+/// @file
+/// @brief Test `putb` works correctly
+///
+/// All content in this file is in the public domain. Use it any way you wish.
+
+#include "test.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ute/attr.h>
+#include <ute/print.h>
+
+static char *buffer;
+static void free_(void *arg UNUSED) { free(buffer); }
+static void fclose_(void *f) { (void)fclose(f); }
+
+TEST("putb") {
+
+  // open a temporary stream
+  register_cleanup(free_, NULL);
+  size_t buffer_size = 0;
+  FILE *const f = open_memstream(&buffer, &buffer_size);
+  ASSERT_NOT_NULL(f);
+  register_cleanup(fclose_, f);
+
+  // try stringizing various things
+  struct {
+    int8_t *data;
+    char in_binary[17];
+  } CASES[] = {
+      {.data = (int8_t[]){0, 0}, .in_binary = "0000000000000000"},
+      {.data = (int8_t[]){1, 0}, .in_binary = "1000000000000000"},
+      {.data = (int8_t[]){0xff, 0}, .in_binary = "1111111100000000"},
+      {.data = (int8_t[]){0x0f, 0xf0}, .in_binary = "1111000000001111"},
+  };
+  for (size_t i = 0; i < sizeof(CASES) / sizeof(CASES[0]); ++i) {
+    const int r = putb(CASES[i].data, 2, f);
+    ASSERT_EQ(r, 16);
+    (void)fflush(f);
+    ASSERT_STREQ(buffer, CASES[i].in_binary);
+
+    // reset the stream
+    memset(buffer, 0, buffer_size);
+    rewind(f);
+  }
+}
