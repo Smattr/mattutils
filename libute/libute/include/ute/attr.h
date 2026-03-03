d2f3fd9cb734d99f8cdb97c4aa7cfeaee5070cd4
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-03-02 17:10:57 -0800

    ute: avoid using '__has…' macros in the same condition as their existence check
    In theory, the C Pre Processor is meant to have shortcut semantics that make
    this kind of logic fine. In practice, Clang 20.1.8 errors out on this kind of
    usage when the given macros are undefined.

--- libute/libute/include/ute/attr.h
+++ libute/libute/include/ute/attr.h
@@ -6,55 +6,78 @@
 #pragma once
 
 /// mark a switch case as intentionally missing a `break` statement
-#if defined(__cplusplus) && defined(__has_cpp_attribute) &&                    \
-    __has_cpp_attribute(fallthrough)
+#if defined(__cplusplus) && defined(__has_cpp_attribute)
+#if __has_cpp_attribute(fallthrough)
 #define FALLTHROUGH [[fallthrough]]
-#elif !defined(__cplusplus) && defined(__has_c_attribute) &&                   \
-    __has_c_attribute(fallthrough)
+#endif
+#endif
+#if !defined(FALLTHROUGH) && !defined(__cplusplus) && defined(__has_c_attribute)
+#if __has_c_attribute(fallthrough)
 #define FALLTHROUGH [[fallthrough]]
-#elif defined(__GNUC__)
+#endif
+#endif
+#if !defined(FALLTHROUGH) && defined(__GNUC__)
 #define FALLTHROUGH __attribute__((fallthrough))
-#else
+#endif
+#ifndef FALLTHROUGH
 #define FALLTHROUGH /* nothing */
 #endif
 
 /// mark a function return value as should-not-be-ignored
-#if defined(__cplusplus) && defined(__has_cpp_attribute) &&                    \
-    __has_cpp_attribute(nodiscard)
+#if defined(__cplusplus) && defined(__has_cpp_attribute)
+#if __has_cpp_attribute(nodiscard)
 #define NODISCARD [[nodiscard]]
-#elif !defined(__cplusplus) && defined(__has_c_attribute) &&                   \
-    __has_c_attribute(nodiscard)
+#endif
+#endif
+#if !defined(NODISCARD) && !defined(__cplusplus) && defined(__has_c_attribute)
+#if __has_c_attribute(nodiscard)
 #define NODISCARD [[nodiscard]]
-#elif defined(__GNUC__)
+#endif
+#endif
+#if !defined(NODISCARD) && defined(__GNUC__)
 #define NODISCARD __attribute__((warn_unused_result))
-#else
+#endif
+#ifndef NODISCARD
 #define NODISCARD /* nothing */
 #endif
 
 /// mark a function as never returning to its caller
-#if defined(__cplusplus) && defined(__has_cpp_attribute) &&                    \
-    __has_cpp_attribute(noreturn)
+#if defined(__cplusplus) && defined(__has_cpp_attribute)
+#if __has_cpp_attribute(noreturn)
 #define NORETURN [[noreturn]]
-#elif !defined(__cplusplus) && defined(__has_c_attribute) &&                   \
-    __has_c_attribute(noreturn)
+#endif
+#endif
+#if !defined(NORETURN) && !defined(__cplusplus) && defined(__has_c_attribute)
+#if __has_c_attribute(noreturn)
 #define NORETURN [[noreturn]]
-#elif defined(__STDC_VERSION__) && __STDC_VERSION__ == 201112L
+#endif
+#endif
+#if !defined(NORETURN) && defined(__STDC_VERSION__)
+#if __STDC_VERSION__ == 201112L
 #define NORETURN _Noreturn
-#elif defined(__GNUC__)
+#endif
+#endif
+#if !defined(NORETURN) && defined(__GNUC__)
 #define NORETURN __attribute__((noreturn))
-#else
+#endif
+#ifndef NORETURN
 #define NORETURN /* nothing */
 #endif
 
 /// mark a variable or function return value as being intentionally not used
-#if defined(__cplusplus) && defined(__has_cpp_attribute) &&                    \
-    __has_cpp_attribute(maybe_unused)
+#if defined(__cplusplus) && defined(__has_cpp_attribute)
+#if __has_cpp_attribute(maybe_unused)
 #define UNUSED [[maybe_unused]]
-#elif !defined(__cplusplus) && defined(__has_c_attribute) &&                   \
-    __has_c_attribute(maybe_unused)
+#endif
+#endif
+#if !defined(UNUSED) && !defined(__cplusplus) && defined(__has_c_attribute)
+#if __has_c_attribute(maybe_unused)
 #define UNUSED [[maybe_unused]]
-#elif defined(__GNUC__)
+#endif
+#endif
+#if !defined(UNUSED) && defined(__GNUC__)
 #define UNUSED __attribute__((unused))
-#else
+#endif
+#ifndef UNUSED
 #define UNUSED /* nothing */
 #endif
