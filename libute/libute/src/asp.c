1721fea2cb3fa569e2513c4e59f5c2270b94a0fa
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-03-05 17:04:29 -0800

    ute: fix UAF in atomic shared pointer interactions
    The path by which this change was arrived at was a wild ride…
    
    As one can see from an XXX comment, I found myself wondering whether two atomics
    could be fused:
    
      // TODO: is it valid to fuse these two atomics?
      if (old_impl.load_count > 0)
        inc_ref(old_impl.ctrl, old_impl.load_count);
      // drop a reference for `dst` we just overwrote
      dec_ref(old_impl.ctrl);
    
    The answer seemed obviously “yes,” as it only reduces the state space. But
    thinking through the edge cases of this opened a can of worms. Suppose we have a
    global shared pointer that holds a value (i.e. is non-null) and has no other
    copies. In this case, its ref count is 1 and the load count is 0. Now consider
    the following interleaving of actions:
      1. Thread A enters `sp_acq`
      2. Thread A increments the load count (load count = 1)
      3. Thread A increments the ref count (ref count = 2)
      4. Thread B enters `sp_store`
      5. Thread B overwrites `dst` with `{src.impl, 0}`
      6. Thread A tries to decrement the load count, but sees the changed control
         block
      7. Thread A decrements the ref count (ref count = 1)
      8. Thread A completes `sp_acq`
      9. Thread A calls `sp_rel`
      10. Thread A decrements the ref count (ref count = 0)
      11. Thread A, seeing a 0 ref count, deallocates the control block
      12. Thread B, seeing a 1 load count, tries to propagate this to the ref count,
          `inc_ref(old_impl.ctrl, old_impl.load_count)`
      13. Thread B derefences `ctrl` in `inc_ref`, which is a Use-After-Free
    
    Because the required timing is quite subtle are rare, it took me a while to
    reproduce this bug. But it did indeed exist. The current change is my attempted
    fix.
    
    After (hopefully) fixing this, I went to look at the other three main sources of
    inspiration:
      1. Anthony Williams’¹
      2. Folly²
      3. Vladislav Tyulbashev’³
    To my surprise, I discovered:
      1. (1) appears to implement essentially the same fix as mine.
      2. (2) appears to implement something equivalent, though it is hard to
         understand if the logic here is specifically addressing this problem or
         handling details of alias references.
      3. (3) does not appear to handle this scenario.
    
    So we now have a fix that, to paraphrase Donald Knuth, I have only run, not
    proven correct. I was already intending to construct a formal model of this, but
    given how counterintuitive and surprising the path to this change was, I think
    we should prioritise this.
    
    ¹ https://github.com/anthonywilliams/atomic_shared_ptr
    ² https://github.com/facebook/folly/blob/main/folly/concurrency/AtomicSharedPtr.h
    ³ https://github.com/vtyulb/AtomicSharedPtr

--- libute/libute/src/asp.c
+++ libute/libute/src/asp.c
@@ -34,6 +34,12 @@
 /// zero-initialized `asp_t` is a valid pointer to null. Similarly a
 /// zero-initialized `sp_t` is a valid reference to null.
 ///
+/// A quirk that is _not_ shown in the above talks is that the reference count
+/// stored in the control block is split into two halves, a reference count and
+/// a load count. This is necessary to avoid a use-after-free (see commit
+/// history of this file), but this requirement is non-obvious and seems to have
+/// been arrived at independently by multiple authors including yours truly.
+///
 /// The asp.h API is phrased such that it is agnostic to how exactly the
 /// “shared” part of “shared pointer” is achieved. We choose to use reference
 /// counting. But in theory, if you are creative enough, you could implement
@@ -47,15 +53,89 @@
 #include <assert.h>
 #include <stdatomic.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <ute/asp.h>
 #include <ute/dword.h>
 
+// uncomment one or more of these to debug unlikely thread interleavings
+// #define DELAY1() sleep(1)
+// #define DELAY2() sleep(1)
+// #define DELAY3() sleep(1)
+
+#ifndef DELAY1
+#define DELAY1()                                                               \
+  do {                                                                         \
+  } while (0)
+#endif
+#ifndef DELAY2
+#define DELAY2()                                                               \
+  do {                                                                         \
+  } while (0)
+#endif
+#ifndef DELAY3
+#define DELAY3()                                                               \
+  do {                                                                         \
+  } while (0)
+#endif
+
+#ifndef __SIZEOF_POINTER__
+#error "sizeof pointer not available"
+#endif
+
+/// signed and unsigned half words
+#if __SIZEOF_POINTER__ == 8
+typedef int32_t ihalf_t;
+typedef uint32_t uhalf_t;
+#define IHALF_MAX INT32_MAX
+#define IHALF_MIN INT32_MIN
+#define UHALF_MAX UINT32_MAX
+#elif __SIZEOF_POINTER__ == 4
+typedef int16_t ihalf_t;
+typedef uint16_t uhalf_t;
+#define IHALF_MAX INT16_MAX
+#define IHALF_MIN INT16_MIN
+#define UHALF_MAX UINT16_MAX
+#error "unimplemented"
+#endif
+
+/// reference count of a control block
+///
+/// This is split into two counts that must be operated on atomically together.
+/// It is only permitted to change `loads` when you hold a reference, thus
+/// preventing `refs` to go to 0 during this. Nothing explicitly enforces this.
+///
+/// The reference count is only logically 0 (allowing clean up) when _both_
+/// `refs` and `loads` are 0.
+///
+/// The overflow assertions later in this file should be impossible to trigger
+/// in practice because they require ≥IHALF_MAX threads operating on the same
+/// shared pointer.
+typedef struct {
+  /// number of outstanding live references to this control block
+  ///
+  /// This is effectively the number of live shared pointers to the pointer this
+  /// count is associated with.
+  uhalf_t refs;
+
+  /// number of live loads of this control block that will be unable to roll
+  /// back
+  ///
+  /// When loading an atomic shared pointer, the load count is first incremented
+  /// and then later decremented. If this counter is relocated, making the
+  /// roll back decrement not possible, the residual load counter value will be
+  /// relocated to here. Note that this value is signed because the order in
+  /// which the decrementer and relocater run is unconstrained; this can go
+  /// negative.
+  ihalf_t loads;
+} rc_t;
+
 struct sp_ctrl {
-  void *value;              ///< the managed underlying pointer
-  void (*dtor)(void *);     ///< optional user-supplied destructor
-  _Atomic size_t ref_count; ///< number of outstanding references
+  void *value;          ///< the managed underlying pointer
+  void (*dtor)(void *); ///< optional user-supplied destructor
+  _Atomic rc_t rc;      ///< outstanding references
 };
 
 /// increment the reference count of a shared pointer
@@ -64,7 +144,34 @@ struct sp_ctrl {
 /// @param by Number of reference to add
 static void inc_ref(sp_ctrl_t *ctrl, size_t by) {
   assert(ctrl != NULL);
-  (void)atomic_fetch_add_explicit(&ctrl->ref_count, by, memory_order_acq_rel);
+  assert(by <= UHALF_MAX && "overflow");
+
+  for (rc_t old = {0};;) {
+    assert(old.refs + by <= UHALF_MAX);
+    const rc_t new = {.refs = old.refs + (uhalf_t)by, .loads = old.loads};
+    if (atomic_compare_exchange_strong_explicit(
+            &ctrl->rc, &old, new, memory_order_acq_rel, memory_order_acquire))
+      break;
+  }
+}
+
+/// propagate an increment of the load count of a shared pointer
+///
+/// @param ctrl Control block to operate on
+/// @param by Number of loads to add
+static void inc_load_ref(sp_ctrl_t *ctrl, size_t by) {
+  assert(ctrl != NULL);
+  assert(by <= (size_t)IHALF_MAX && "overflow");
+
+  rc_t old = atomic_load_explicit(&ctrl->rc, memory_order_acquire);
+  while (true) {
+    assert(old.refs > 0 && "changing load count while not holding a reference");
+    assert(IHALF_MAX - (ihalf_t)by >= old.loads && "overflow");
+    const rc_t new = {.refs = old.refs, .loads = old.loads + (ihalf_t)by};
+    if (atomic_compare_exchange_strong_explicit(
+            &ctrl->rc, &old, new, memory_order_acq_rel, memory_order_acquire))
+      break;
+  }
 }
 
 /// decrement the reference count of a shared pointer by 1
@@ -73,16 +180,37 @@ static void inc_ref(sp_ctrl_t *ctrl, size_t by) {
 static void dec_ref(sp_ctrl_t *ctrl) {
   assert(ctrl != NULL);
 
-  const size_t old =
-      atomic_fetch_sub_explicit(&ctrl->ref_count, 1, memory_order_acq_rel);
-  assert(old > 0 && "dropping a reference that was not held");
+  rc_t old = atomic_load_explicit(&ctrl->rc, memory_order_acquire);
+  while (true) {
+    assert(old.refs > 0 && "dropping a reference that was not held");
+    const rc_t new = {.refs = old.refs - 1, .loads = old.loads};
+    if (atomic_compare_exchange_strong_explicit(
+            &ctrl->rc, &old, new, memory_order_acq_rel, memory_order_acquire))
+      break;
+  }
 
   // if we just dropped the last reference, clean up
-  if (old == 1) {
-    if (ctrl->dtor != NULL) {
+  if (old.refs == 1 && old.loads == 0) {
+    if (ctrl->dtor != NULL)
       ctrl->dtor(ctrl->value);
-    }
     free(ctrl);
+  }
+}
+
+/// decrement the propagated load count of a shared pointer by 1
+///
+/// @param ctrl Control block to operate on
+static void dec_load_ref(sp_ctrl_t *ctrl) {
+  assert(ctrl != NULL);
+
+  rc_t old = atomic_load_explicit(&ctrl->rc, memory_order_acquire);
+  while (true) {
+    assert(old.refs > 0 && "changing load count while not holding a reference");
+    assert(old.loads > IHALF_MIN && "overflow");
+    const rc_t new = {.refs = old.refs, .loads = old.loads - 1};
+    if (atomic_compare_exchange_strong_explicit(
+            &ctrl->rc, &old, new, memory_order_acq_rel, memory_order_acquire))
+      break;
   }
 }
 
@@ -155,6 +283,8 @@ sp_t sp_acq(asp_t *asp) {
     }
   }
 
+  DELAY1();
+
   asp_impl_t impl = asp2impl(old);
   assert(impl.ctrl != NULL && "non-null pointer has no control block");
 
@@ -173,7 +303,8 @@ sp_t sp_acq(asp_t *asp) {
     const asp_impl_t updated = asp2impl(old);
     if (impl.ctrl != updated.ctrl) {
       // XXX: this is the counter-intuitive path
-      dec_ref(impl.ctrl);
+      DELAY2();
+      dec_load_ref(impl.ctrl);
       break;
     }
     impl = updated;
@@ -211,7 +342,8 @@ void sp_store(asp_t *dst, sp_t src) {
   if (old_impl.ctrl != NULL) {
     // TODO: is it valid to fuse these two atomics?
     if (old_impl.load_count > 0)
-      inc_ref(old_impl.ctrl, old_impl.load_count);
+      inc_load_ref(old_impl.ctrl, old_impl.load_count);
+    DELAY3();
     // drop a reference for `dst` we just overwrote
     dec_ref(old_impl.ctrl);
   }
