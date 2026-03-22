af1b050a7848272e2a3ee8058e2c6ed190f3e6e7
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-03-22 09:46:41 -0700

    ute: abstraction for 'typeof'

---
+++ libute/libute/include/ute/typeof.h
@@ -0,0 +1,19 @@
+/// @file
+/// @brief Compatibility abstraction for `typeof` operator
+///
+/// All content in this file is in the public domain. Use it any way you wish.
+
+#pragma once
+
+#ifdef __STDC_VERSION__
+#if __STDC_VERSION__ == 202311L
+#define TYPEOF typeof
+#endif
+#endif
+#if !defined(TYPEOF) &&                                                        \
+    (defined(__GNUC__) || defined(_MSC_VER)) // Clang, GCC, MSVC
+#define TYPEOF __typeof__
+#endif
+#ifndef TYPEOF
+#error "no support for typeof"
+#endif
