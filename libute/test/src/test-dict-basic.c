8d5420b4507c67ce2b79419f7f96542bec2b60e3
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-04-22 17:11:30 +1000

    ute: add a test case for object types in dictionaries

--- libute/test/src/test-dict-basic.c
+++ libute/test/src/test-dict-basic.c
@@ -6,6 +6,7 @@
 #include "test.h"
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <ute/dict.h>
 
 /// freeing an empty dictionary should be OK and should not leak memory
@@ -58,4 +59,59 @@ TEST("int→int dict") {
   }
 
   DICT_FREE(&ints);
+}
+
+/// a wider type to an object type
+TEST("int64_t→struct dict") {
+  struct foo {
+    int x;
+    long y;
+    char z;
+  };
+
+  DICT(int64_t, struct foo) xs = {0};
+
+  for (int i = 0; i < 10; ++i) {
+    const int r = DICT_SET(&xs, i, ((struct foo){.x = i, .y = i}));
+    ASSERT_EQ(r, 0);
+    ASSERT_EQ(DICT_SIZE(&xs), (size_t)i + 1);
+
+    for (int j = 0; j < 10; ++j) {
+      const bool present = DICT_CONTAINS(&xs, j);
+      const struct foo *const v = DICT_GET(&xs, j);
+      if (j <= i) {
+        ASSERT(present);
+        ASSERT_NOT_NULL(v);
+        ASSERT_EQ(v->x, j);
+        ASSERT_EQ(v->y, j);
+        ASSERT_EQ(v->z, 0);
+      } else {
+        ASSERT(!present);
+        ASSERT_NULL(v);
+      }
+    }
+  }
+
+  for (int i = 0; i < 10; ++i) {
+    const bool r = DICT_REMOVE(&xs, i);
+    ASSERT(r);
+    ASSERT_EQ(DICT_SIZE(&xs), 10 - (size_t)i - 1);
+
+    for (int j = 0; j < 10; ++j) {
+      const bool present = DICT_CONTAINS(&xs, j);
+      const struct foo *const v = DICT_GET(&xs, j);
+      if (j <= i) {
+        ASSERT(!present);
+        ASSERT_NULL(v);
+      } else {
+        ASSERT(present);
+        ASSERT_NOT_NULL(v);
+        ASSERT_EQ(v->x, j);
+        ASSERT_EQ(v->y, j);
+        ASSERT_EQ(v->z, 0);
+      }
+    }
+  }
+
+  DICT_FREE(&xs);
 }
